887ac88c701d193444d9a817bd9f7028259b1f61
psaavala <c3sahe00@students.oamk.fi>
2024-11-28 13:55:17 +0200

    added support for sending data to endpoint

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var app = express();
 
 const port = process.env.PORT || 3001;
 
+app.use(express.urlencoded({extended: true}));
+
 app.listen(port, () =>{
     console.log('server is running');
 });
 
-app.get('/', (req, res)=> res.send('hello world'));
 
 app.get('/movie', (req, res) => res.send('Movies endpoint'));
 app.get('/uuser', (req, res) => res.send('Users endpoint'));
@@ -15,3 +16,8 @@ app.get('/review', (req, res) => res.send('Reviews endpoint'));
 app.get('/genre', (req, res) => res.send('genre endpoint'));
 app.get('/fav_movie', (req, res) => res.send('fav_movie endpoint'));
 
+app.post('/uuser', (req, res) =>{
+    let user = req.body;
+    console.log(user);
+    res.send();
+});
